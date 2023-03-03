ca3a1d43e0515aafbb2ad6aabaa5bae1b09f3ac5
Jordan <jordanporter94@outlook.com>
2023-03-03 21:00:51 +0000

    GPT config

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-IConfigurationBuilder builder = new ConfigurationBuilder();
+IConfigurationBuilder builder = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+.AddUserSecrets(typeof(Program).Assembly, optional: true);
 
-
+IConfigurationRoot config = builder.Build();
 
 if (args.Length > 0)
 {
     HttpClient client = new HttpClient();
 
-    client.DefaultRequestHeaders.Add("authorization", "Bearer ");
+    client.DefaultRequestHeaders.Add("authorization", "Bearer " + config["GptApiKey"]);
 }
 else
 {
-    Console.WriteLine("----> You need to provide some input");
+    Console.WriteLine("----> You need to provide some input ");
 }
