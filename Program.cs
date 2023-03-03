e788e6defa42ad7cd3933339182b51051b36e0a5
Jordan <jordanporter94@outlook.com>
2023-03-03 20:51:54 +0000

    Add config packages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationBuilder builder = new ConfigurationBuilder();
+
+
+
+if (args.Length > 0)
+{
+    HttpClient client = new HttpClient();
+
+    client.DefaultRequestHeaders.Add("authorization", "Bearer ");
+}
+else
+{
+    Console.WriteLine("----> You need to provide some input");
+}
